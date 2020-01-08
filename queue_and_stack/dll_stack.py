8f1170462d1299fbab53d2f31a74d33b1f6fb450
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-01-08 14:09:49 +0800

    Complete Stack data structure

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,30 @@
-import sys
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+            # ANSWER: Push and Pop are have O(1) time complexity
+        self.storage = None
 
     def push(self, value):
-        pass
+        if self.storage is None:
+            self.storage = DoublyLinkedList(ListNode(value))
+            self.size = self.storage.length
+
+        else:
+            self.storage.add_to_tail(value)
+            self.size = self.storage.length
 
     def pop(self):
-        pass
+        if self.storage is None:
+            return None
+
+        else:
+            node = self.storage.remove_from_tail()
+            self.size = self.storage.length
+            return node
+
 
     def len(self):
-        pass
+        return self.size
