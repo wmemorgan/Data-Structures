848ae45a32b31ccf9594ee5d07f839ce7698b4b8
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-01-08 14:21:08 +0800

    Refactor stack data structure

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -4,26 +4,26 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-            # ANSWER: Push and Pop are have O(1) time complexity
+            # ANSWER: Push and Pop have O(1) time complexity
         self.storage = None
 
     def push(self, value):
         if self.storage is None:
             self.storage = DoublyLinkedList(ListNode(value))
-            self.size = self.storage.length
 
         else:
             self.storage.add_to_tail(value)
-            self.size = self.storage.length
+        
+        self.size = self.storage.length
 
     def pop(self):
         if self.storage is None:
             return None
 
         else:
-            node = self.storage.remove_from_tail()
+            tail = self.storage.remove_from_tail()
             self.size = self.storage.length
-            return node
+            return tail
 
 
     def len(self):
